1da081748de32a5f65373ef98c48cb0dcafd2928
RVS DINESH <57357275+rvsdinesh@users.noreply.github.com>
2021-08-10 20:13:47 +0530

    Book Management API

---
+++ requirements.js
@@ -0,0 +1,50 @@
+
+//Requirement for our project
+
+//We are a book management company
+
+//BOOKS
+//ISBN, title, pub date, language, num page, author[], category[]
+
+//AUTHORS
+//id, name, books[]
+
+//PUBLICATIONS
+//id, name, books[]
+
+//We have to design and code an API over this .
+
+//1. BOOKS
+//We need an API :-
+//To get all the books -DONE
+//To get specific book -DONE
+//To get a list of books based on category  -DONE
+//To get a list of books based on languages - YOUR TASK -DONE
+
+//2. AUTHORS
+//We need an API :-
+//To get all the authors  -DONE
+//To get a specific author based on id -YOUR TASK -DONE
+//To get a list of authors based on books -DONE
+
+//3. PUBLICATIONS
+//We need an API :-
+//To get all the publications - DONE
+//To get a specific publication - YOUR  TASK -DONE
+//To get a list of publications based on a book - YOUR TASK -DONE
+
+
+//POST
+//1.ADD NEW BOOK -DONE
+//2.ADD NEW PUBLICATION -DONE
+//3.ADD NEW AUTHOR -DONE
+
+
+//PUT
+//1. Update Book database if Publication has changed -DONE
+
+
+//DELETE
+//1.Delete a book -DONE
+//2.Delete author from book -YOUR TASK -DONE
+//3.Delete author from book and related book from author -DONE
